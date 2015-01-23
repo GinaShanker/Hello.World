570eccaf3a58ab9a6f3b0761ce60b9e45b6513ed
Gina Kaur Shanker <P1322610X@GH583-012.lec-admin.dmu.ac.uk>
2015-01-23 14:23:32 +0000

    created maths_functions and two_cities

---
+++ maths_functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    i = 10;
+    j = 20;  
+    printf("i+j = %i\n", i+j);
+    printf("i-j = %i\n", i-j);
+    printf("i*j = %i\n", i*j);
+    printf("i/j = %i\n", i/j);
+    return 0;
+    
+}
